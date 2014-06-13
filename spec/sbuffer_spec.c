74b3b5ed8701c16ce6e8ce6c3a443e7ff78ef7df
John Mettraux <jmettraux@gmail.com>
2014-06-14 07:22:33 +0900

    implement flu_sbputs_n()

--- spec/sbuffer_spec.c
+++ spec/sbuffer_spec.c
@@ -34,5 +34,22 @@ context "sbuffer"
       ensure(((char)c) == 'a');
     }
   }
+
+  describe "flu_sbputs_n()"
+  {
+    it "prints part of a string to a flu_sbuffer"
+    {
+      flu_sbuffer *b = flu_sbuffer_malloc();
+
+      flu_sbputs_n(b, "hello", 4);
+      flu_sbputs_n(b, "  ", 1);
+      flu_sbputs_n(b, "freeze ", 99);
+      flu_sbputs_n(b, "will\0iam", 7);
+
+      char *s = flu_sbuffer_to_string(b);
+
+      ensure(s ===f "hell freeze will");
+    }
+  }
 }
 
