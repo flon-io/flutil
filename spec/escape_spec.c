3ba9d4fcd247b1d0a80c5c8336d0252cf1b260c4
John Mettraux <jmettraux@gmail.com>
2014-06-14 16:41:08 +0900

    move in dja_ escape functions

---
+++ spec/escape_spec.c
@@ -0,0 +1,57 @@
+
+//
+// specifying flutil
+//
+// Sat Jun  7 09:41:22 JST 2014
+//
+
+#include "flutil.h"
+
+
+context "escapes"
+{
+  describe "flu_escape()"
+  {
+    it "escapes newlines"
+    {
+      ensure(flu_escape("new\nline") ===f "new\\nline");
+    }
+    it "escapes double quotes"
+    {
+      ensure(flu_escape("double \" quotes") ===f "double \\\" quotes");
+    }
+    it "doesn't escape if not necessary"
+    {
+      ensure(flu_escape("fuji山") ===f "fuji山");
+    }
+  }
+
+  describe "flu_unescape()"
+  {
+    it "unescapes back slashes"
+    {
+      ensure(flu_unescape("back\\\\slash") ===f "back\\slash");
+    }
+    it "unescapes double quotes"
+    {
+      ensure(flu_unescape("double\\\"quote") ===f "double\"quote");
+    }
+    it "unescapes tabs"
+    {
+      ensure(flu_unescape("a\\ttab") ===f "a\ttab");
+    }
+    it "unescapes \\uxxxx sequences"
+    {
+      ensure(flu_unescape("fuji\\u5c71") ===f "fuji山");
+    }
+    it "unescapes low \\uxxxx sequences"
+    {
+      ensure(flu_unescape("old \\u0066ool") ===f "old fool");
+    }
+    it "leaves unknown escapes as is"
+    {
+      ensure(flu_unescape("that's \\zorro") ===f "that's \\zorro");
+    }
+  }
+}
+
