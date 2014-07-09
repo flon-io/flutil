72ee344265e7547a3034bee1b327133bd44acdd1
John Mettraux <jmettraux@gmail.com>
2014-07-09 09:35:17 +0900

    emphasize "flu_list" over "colls"

--- src/flutil.h
+++ src/flutil.h
@@ -36,18 +36,15 @@
 //
 // str functions
 
-/*
- * Returns 1 if the string s ends with the end string. Returns 0 else.
+/* Returns 1 if the string s ends with the end string. Returns 0 else.
  */
 int flu_strends(const char *s, const char *end);
 
-/*
- * Returns a copy of the string, trimmed on the right.
+/* Returns a copy of the string, trimmed on the right.
  */
 char *flu_strrtrim(const char *s);
 
-/*
- * Returns a trimmed copy of the string, left and right.
+/* Returns a trimmed copy of the string, left and right.
  */
 char *flu_strtrim(const char *s);
 
@@ -61,60 +58,50 @@ typedef struct flu_sbuffer {
   size_t len;
 } flu_sbuffer;
 
-/*
- * Creates a buffer (its stream) and returns a pointer to it.
+/* Creates a buffer (its stream) and returns a pointer to it.
  */
 flu_sbuffer *flu_sbuffer_malloc();
 
-/*
- * Frees the sbuffer (closes the stream and frees the string if necessary).
+/* Frees the sbuffer (closes the stream and frees the string if necessary).
  */
 void flu_sbuffer_free(flu_sbuffer *b);
 
-/*
- * Formats input and writes into buffer. Takes a va_list.
+/* Formats input and writes into buffer. Takes a va_list.
  */
 int flu_sbvprintf(flu_sbuffer *b, const char *format, va_list ap);
 
-/*
- * Formats input and writes into buffer. Called like printf is called.
+/* Formats input and writes into buffer. Called like printf is called.
  */
 int flu_sbprintf(flu_sbuffer *b, const char *format, ...);
 
-/*
- * Puts a single char to the buffer. It's the equivalent of putc.
+/* Puts a single char to the buffer. It's the equivalent of putc.
  */
 int flu_sbputc(flu_sbuffer *b, int c);
 
-/*
- * The equivalent of fputs(). Warning: the buffer is the first argument.
+/* The equivalent of fputs(). Warning: the buffer is the first argument.
  */
 int flu_sbputs(flu_sbuffer *b, const char *s);
 
-/*
- * Puts the n first chars of s to the buffer. Return a non-negative int
+/* Puts the n first chars of s to the buffer. Return a non-negative int
  * on success, EOF in case of error.
  * Stops upon encountering a \0.
  */
 int flu_sbputs_n(flu_sbuffer *b, const char *s, size_t n);
 
-/*
- * Closes the buffer (stream) which causes the string to be made available.
+/* Closes the buffer (stream) which causes the string to be made available.
  *
  * Doesn't not free the buffer, it still is around for further reading
  * of b->string.
  */
 int flu_sbuffer_close(flu_sbuffer *b);
 
-/*
- * Closes the buffer, frees it and returns the pointer to the produced string.
+/* Closes the buffer, frees it and returns the pointer to the produced string.
  *
  * Returns NULL in case of issue (buffer should be free anyway).
  */
 char *flu_sbuffer_to_string(flu_sbuffer *b);
 
-/*
- * Wraps the sbuffer operations in a single call, yielding the result string.
+/* Wraps the sbuffer operations in a single call, yielding the result string.
  *
  * Returns NULL in case of issue.
  */
@@ -124,8 +111,7 @@ char *flu_sprintf(const char *format, ...);
 //
 // die
 
-/*
- * Makes the process exit with the given exit_value. Right before
+/* Makes the process exit with the given exit_value. Right before
  * that it does perror(msg) where msg is composed with the given format
  * and arguments.
  */
@@ -135,23 +121,23 @@ void flu_die(int exit_value, const char *format, ...);
 //
 // escape
 
-/*
- * Returns an escaped copy of the given string.
+/* Returns an escaped copy of the given string.
  * Only escapes \ " \b \f \n \r \t. It doesn't escape UTF-8 chars (the
  * ones above ASCII).
  */
 char *flu_escape(const char *s);
 char *flu_n_escape(const char *s, size_t n);
 
-/*
- * Returns an unescaped copy of the given string.
+/* Returns an unescaped copy of the given string.
  */
 char *flu_unescape(const char *s);
 char *flu_n_unescape(const char *s, size_t n);
 
 
 //
-// colls
+// flu_list
+//
+// a minimal list/stack/set with no ambition
 
 typedef struct flu_node {
   struct flu_node *next;
