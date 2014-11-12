7fb22288e75c2dafab6afbbdd9e43503a75cae2b
John Mettraux <jmettraux@gmail.com>
2014-11-12 21:50:08 +0900

    add specs for padding

--- spec/flu64_spec.c
+++ spec/flu64_spec.c
@@ -23,7 +23,45 @@ context "flu64"
     }
 
     it "encodes and pads (=)"
+    {
+      char *in = "Man is d";
+      char *out = calloc(strlen(in) * 2, sizeof(char));
+
+      flu64_do_encode(in, strlen(in), out);
+
+      expect(out ===f "TWFuIGlzIGQ=");
+    }
+
     it "encodes and pads (==)"
+    {
+      char *in = "Man is ";
+      char *out = calloc(strlen(in) * 2, sizeof(char));
+
+      flu64_do_encode(in, strlen(in), out);
+
+      expect(out ===f "TWFuIGlzIA==");
+    }
+
+    it "encodes it all"
+    {
+      char *in =
+        "Man is distinguished, not only by his reason, but by this singular "
+        "passion from other animals, which is a lust of the mind, that by a "
+        "perseverance of delight in the continued and indefatigable "
+        "generation of knowledge, exceeds the short vehemence of any carnal "
+        "pleasure.";
+      char *out = calloc(strlen(in) * 2, sizeof(char));
+
+      flu64_do_encode(in, strlen(in), out);
+
+      expect(out ===f ""
+        "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1d"
+        "CBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aG"
+        "ljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2U"
+        "gb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdl"
+        "bmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY"
+        "2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=");
+    }
   }
 
   describe "flu64_encode()"
@@ -36,19 +74,4 @@ context "flu64"
     it "decodes"
   }
 }
-
-//  char *lev =
-//    "Man is distinguished, not only by his reason, but by this singular "
-//    "passion from other animals, which is a lust of the mind, that by a "
-//    "perseverance of delight in the continued and indefatigable generation "
-//    "of knowledge, exceeds the short vehemence of any carnal pleasure.";
-//  //char *lev = "Man is";
-//
-//  char *out =
-//    "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0"
-//    "aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1"
-//    "c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0"
-//    "aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdl"
-//    "LCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
-//  //char *out = "TWFuIGlz";
 
